e3f93c429e0ad80d198f3c5e7e898d68df978895
Eka Dharma <111685319+ekadk@users.noreply.github.com>
2025-01-10 21:42:32 +0800

    Feat/user tasks table (#3)
    * add: table component
    
    * add: dummy data
    
    * add: user tasks table

--- app/u/page.tsx
+++ app/u/page.tsx
@@ -1,7 +1,12 @@
+import { tasks } from "@/lib/dummy";
+import { columns, Payment } from "./columns";
+import { DataTable } from "./data-table";
+
 export default function DashboardPage() {
   return (
     <main className="px-20 py-10 flex flex-col gap-8">
       <h1 className="font-black text-2xl">Tasks Overview</h1>
+      <DataTable columns={columns} data={tasks} />
     </main>
   );
 }
---
+++ lib/dummy.ts
@@ -0,0 +1,124 @@
+import { Task } from "@/app/u/columns";
+
+export const tasks: Task[] = [
+  {
+    id: 1,
+    name: "Complete project proposal",
+    status: 2,
+    priority: 2,
+  },
+  {
+    id: 2,
+    name: "Review team feedback",
+    status: 0,
+    priority: 1,
+  },
+  {
+    id: 3,
+    name: "Finalize budget report",
+    status: 3,
+    priority: 2,
+  },
+  {
+    id: 4,
+    name: "Schedule meeting with client",
+    status: 2,
+    priority: 2,
+  },
+  {
+    id: 5,
+    name: "Update website content",
+    status: 0,
+    priority: 0,
+  },
+  {
+    id: 6,
+    name: "Prepare presentation slides",
+    status: 2,
+    priority: 1,
+  },
+  {
+    id: 7,
+    name: "Organize team workshop",
+    status: 0,
+    priority: 0,
+  },
+  {
+    id: 8,
+    name: "Research new market trends",
+    status: 3,
+    priority: 1,
+  },
+  {
+    id: 9,
+    name: "Respond to client emails",
+    status: 2,
+    priority: 2,
+  },
+  {
+    id: 10,
+    name: "Develop marketing strategy",
+    status: 0,
+    priority: 2,
+  },
+  {
+    id: 11,
+    name: "Plan company retreat",
+    status: 0,
+    priority: 0,
+  },
+  {
+    id: 12,
+    name: "Fix reported app bugs",
+    status: 0,
+    priority: 2,
+  },
+  {
+    id: 13,
+    name: "Design new product prototype",
+    status: 3,
+    priority: 1,
+  },
+  {
+    id: 14,
+    name: "Update social media calendar",
+    status: 0,
+    priority: 0,
+  },
+  {
+    id: 15,
+    name: "Prepare monthly newsletter",
+    status: 0,
+    priority: 1,
+  },
+  {
+    id: 16,
+    name: "Review vendor contracts",
+    status: 3,
+    priority: 2,
+  },
+  {
+    id: 17,
+    name: "Conduct employee survey",
+    status: 0,
+    priority: 0,
+  },
+  {
+    id: 18,
+    name: "Analyze sales performance",
+    status: 3,
+    priority: 1,
+  },
+  {
+    id: 19,
+    name: "Draft quarterly report",
+    status: 0,
+    priority: 2,
+  },
+  {
+    id: 20,
+    name: "Brainstorm campaign ideas",
+    status: 0,
+    priority: 1,
+  },
+];
